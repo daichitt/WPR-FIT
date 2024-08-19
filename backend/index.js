151bb2f564d29c28f79ee5556b36a56020283bbf
daichit <gene.sixteen.000925@outlook.jp>
2024-08-19 11:16:25 +0700

    [backend] Hello World

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ import { PORT } from './config.js';
 
 
 const app = express();
+app.get('/', (req, res) =>  {
+    console.log(res);
+    res.send('Hello World');
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening to port: ${PORT}`);
